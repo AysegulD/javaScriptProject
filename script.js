39b6617449b9f0127b928d8b17ce7694377f30ae
Aysegul Demir <aydemgul@hotmail.com>
2021-12-16 21:41:33 +0000

    1612-lastchanges

--- script.js
+++ script.js
@@ -1,64 +1,93 @@
-const mscContainer=document.querySelector('.container')
-const playBtn=document.querySelector('#play')
-const prev=document.querySelector('#previous')
-const frwrd=document.querySelector('#forward')
-const audio=document.querySelector('#audio')
-const title=document.querySelector('#title')
-const artist=document.querySelector('#singer')
-const cover=document.querySelector('#cover')
-const slide=document.querySelector('.slide')
+const mscContainer = document.querySelector('.container')
+const playBtn = document.querySelector('#play')
+const prev = document.querySelector('#previous')
+const frwrd = document.querySelector('#forward')
+const audio = document.querySelector('#audio')
+const title = document.querySelector('#title')
+const artist = document.querySelector('#singer')
+const cover = document.querySelector('#cover')
+const progressRange = document.querySelector('#progressRange')
+const durationTime = document.querySelector('#durationTime')
+const currentTime = document.querySelector('#currentTime')
+const slide = document.querySelector('.slide')
 
 
-const songs=['Instant Crush','Hysteria','Wonderwall']
-const artists=['Daft Punk','Muse','Oasis']
+const songs = ['Instant Crush','Hysteria','Wonderwall']
+const artists = ['Daft Punk','Muse','Oasis']
 
-let songIndex=0;
+let songIndex = 0;
 
-loadSong(songs[songIndex])
-function loadSong(song){
-    title.textContent=songs[songIndex]
-   artist.textContent=artists[songIndex]
-    audio.src= `music/${song}.mp3`
-    cover.src=`images/${song}.jpg`
+loadSong (songs[songIndex])
+function loadSong (song) {
+    title.textContent = songs[songIndex]
+   artist.textContent = artists[songIndex]
+    audio.src = `music/${song}.mp3`
+    cover.src =`images/${song}.jpg`
+   bodyImg.src = `images/${song}.jpg`
 }
 
-function playSong(){
+function playSong () {
     mscContainer.classList.add('play')
     playBtn.querySelector('i.fas').classList.remove('fa-play-circle')
     playBtn.querySelector('i.fas').classList.add('fa-pause')
     audio.play()
+    
 }
-function pauseSong(){
+function pauseSong () {
     mscContainer.classList.remove('play')
     playBtn.querySelector('i.fas').classList.add('fa-play-circle')
     playBtn.querySelector('i.fas').classList.remove('fa-pause')
     audio.pause()
 }
-function frwrdSong(){
+
+audio.addEventListener('ended',frwrdSong)
+
+function frwrdSong () {
     songIndex++
-if(songIndex>songs.length-1){
+    console.log(songIndex)
+if (songIndex > songs.length-1) {
     songIndex = 0
     }
-    loadSong(songs[songIndex])  
-    playSong()
+    loadSong (songs[songIndex])  
+    playSong ()
 }
 
-function previousSong(){
+function previousSong () {
     songIndex--
-if(songIndex<0){
+if (songIndex < 0) {
     songIndex = songs.length -1
     }
-    loadSong(songs[songIndex])  
-    playSong()
+    loadSong (songs[songIndex])  
+    playSong ()
 }
+function progressSong () {
+    progressRange.max = audio.duration;
+    progressRange.value = audio.currentTime;
+    currentTime.textContent  = updateTime (audio.currentTime)
+    durationTime.textContent = updateTime (audio.duration)
+}
+setInterval (progressSong,1000)
 
-slide.addEventListener('input',function(){
+function updateTime (sec) {
+    let min = Math.floor (sec / 60)
+    let seconds = Math.floor (sec - min * 60)
+    if (seconds < 10) {
+        seconds = `0${seconds}`
+    }
+    return `${min}:${seconds}`
+}
+function changeProgressBar () {
+    audio.currentTime = progressRange.value
+}
+progressRange.addEventListener ('click',changeProgressBar)
+
+slide.addEventListener ('input',function () {
     audio.volume = slide.value / 100;
 });
 
-playBtn.addEventListener('click',()=>{
-    const isPlaying= mscContainer.classList.contains('play')
-    if(isPlaying){
+playBtn.addEventListener('click',() => {
+    const isPlaying = mscContainer.classList.contains('play')
+    if (isPlaying){
         pauseSong()
     }else{
         playSong()
@@ -67,3 +96,4 @@ playBtn.addEventListener('click',()=>{
 
 prev.addEventListener('click',previousSong)
 frwrd.addEventListener('click',frwrdSong)
+
